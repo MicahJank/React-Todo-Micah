a8f44cdde9643e65cfc3dde856ef12c32384da97
MicahJank <MicahJank@users.noreply.github.com>
2019-09-04 19:21:19 -0500

    began working on styling and setting up the code differently based on how i want the layout to be

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 import todos from './todos.js';
 
+import { Container } from 'semantic-ui-react';
+import styled from 'styled-components';
+
 import TodoForm from './components/TodoComponents/TodoForm.js';
 import TodoList from './components/TodoComponents/TodoList.js';
+
+
+const TopBar = styled.div`
+  display: flex;
+  padding: 50px;
+`;
+
+const StatsBar = styled.div`
+  display: flex;
+  padding: 15px;
+`;
+
+const AddTaskContainer = styled.div`
+  margin: 15px 10px;
+`;
+
+const UncompletedTasks = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CompletedTasks = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -62,14 +90,43 @@ class App extends React.Component {
     })
   }
 
+  // completedTodos will be called inside of the todoList component as a prop, important to note however that we are NOT passing this FUNCTION down
+  // as a prop, but rather we will be passing down the RESULT of calling this function as a prop. That way we can re use the TodoList component and have it render both the
+  // completed todos and the uncompleted todos separately.
+  completedTodos = (isCompleted) => {
+    if(isCompleted) {
+      return this.state.todos.filter(todo => todo.completed === true);
+    } else {
+      return this.state.todos.filter(todo => todo.completed === false);
+    }
+    
+  }
+
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-        <h3>Todo's</h3>
-        <TodoForm handleChange={this.handleFormChanges} handleSubmit={this.handleFormSubmit} clearCompleted={this.clearCompleted} />
-        <TodoList todos={this.state.todos} toggleCompleted={this.toggleCompleted} />
-      </div>
+      <Container>
+        <TopBar>
+          <h2>Lambda ToDo</h2>
+        </TopBar>
+        
+        <StatsBar></StatsBar>
+        
+        <AddTaskContainer>
+          <TodoForm handleChange={this.handleFormChanges} handleSubmit={this.handleFormSubmit} clearCompleted={this.clearCompleted} />
+        </AddTaskContainer>
+
+        <UncompletedTasks>
+          <TodoList todos={this.completedTodos(false)} toggleCompleted={this.toggleCompleted} />
+        </UncompletedTasks>
+
+        <CompletedTasks>
+          <TodoList todos={this.completedTodos(true)} toggleCompleted={this.toggleCompleted} />
+        </CompletedTasks>
+
+      
+        
+      </Container>
     );
   }
 }
